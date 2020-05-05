4b0bdd7da43fe414df4c85c1ede95ef3801c3f38
Jake Luciani <jake@datastax.com>
2020-05-05 10:09:55 -0400

    Fix packaging

--- make-package.sh
+++ make-package.sh
@@ -7,33 +7,36 @@ if [ "$VERSION" == "" ]; then
   exit 1
 fi
 
+PACKAGE_DIR=package-$VERSION
 PROJECT_DIR_NAME=datastax-mcac-agent-$VERSION
 
+mkdir $PACKAGE_DIR
+
 mvn -DskipTests clean package -Drevision=$VERSION
-mkdir -p $PROJECT_DIR_NAME/config
-cp config/collectd.conf.tmpl $PROJECT_DIR_NAME/config
-cp config/metrics-collector.yaml $PROJECT_DIR_NAME/config
+mkdir -p $PACKAGE_DIR/$PROJECT_DIR_NAME/config
+cp config/collectd.conf.tmpl $PACKAGE_DIR/$PROJECT_DIR_NAME/config
+cp config/metrics-collector.yaml $PACKAGE_DIR/$PROJECT_DIR_NAME/config
 
-cp README.md $PROJECT_DIR_NAME
-cp LICENSE.md $PROJECT_DIR_NAME
-mkdir $PROJECT_DIR_NAME/target
-cp -r target/collectd $PROJECT_DIR_NAME/target
-cp target/datastax-mcac-agent-$VERSION-SNAPSHOT.jar $PROJECT_DIR_NAME/target
-mv $PROJECT_DIR_NAME/target $PROJECT_DIR_NAME/lib
-mv $PROJECT_DIR_NAME/lib/datastax-mcac-agent-$VERSION-SNAPSHOT.jar $PROJECT_DIR_NAME/lib/datastax-mcac-agent.jar
-tar zcvf $PROJECT_DIR_NAME.tar.gz $PROJECT_DIR_NAME
-zip $PROJECT_DIR_NAME.zip $(tar ztf $PROJECT_DIR_NAME.tar.gz)
+cp README.md $PACKAGE_DIR/$PROJECT_DIR_NAME
+cp LICENSE.txt $PACKAGE_DIR/$PROJECT_DIR_NAME
+mkdir $PACKAGE_DIR/$PROJECT_DIR_NAME/lib
+cp -r target/collectd $PACKAGE_DIR/$PROJECT_DIR_NAME/lib
+cp target/datastax-mcac-agent-$VERSION.jar $PACKAGE_DIR/$PROJECT_DIR_NAME/lib/datastax-mcac-agent.jar
+cp -r scripts $PACKAGE_DIR/$PROJECT_DIR_NAME/
+tar zcvf $PACKAGE_DIR/$PROJECT_DIR_NAME.tar.gz $PACKAGE_DIR/$PROJECT_DIR_NAME
+zip $PACKAGE_DIR/$PROJECT_DIR_NAME.zip $(tar ztf $PACKAGE_DIR/$PROJECT_DIR_NAME.tar.gz)
 
-cd dashboards
+pushd .
+cd dashboards/grafana
 ./make-dashboards.sh
-
+popd
 
 DASHBOARD_DIR_NAME=datastax-mcac-dashboards-$VERSION
-mkdir -p $DASHBOARD_DIR_NAME/grafana
-cp dashboards/docker-compose.yml $DASHBOARD_DIR_NAME
-cp -r dashboards/prometheus $DASHBOARD_DIR_NAME
-cp dashboards/grafana/prometheus-datasource.yaml $DASHBOARD_DIR_NAME/grafana
-cp dashboards/grafana/dashboards.yaml $DASHBOARD_DIR_NAME/grafana
-cp -r dashboards/grafana/generated-dashboards $DASHBOARD_DIR_NAME/grafana
-tar zcvf $DASHBOARD_DIR_NAME.tar.gz $DASHBOARD_DIR_NAME
-zip $DASHBOARD_DIR_NAME.zip $(tar ztf $DASHBOARD_DIR_NAME.tar.gz)
+mkdir -p $PACKAGE_DIR/$DASHBOARD_DIR_NAME/grafana
+cp dashboards/docker-compose.yaml $PACKAGE_DIR/$DASHBOARD_DIR_NAME
+cp -r dashboards/prometheus $PACKAGE_DIR/$DASHBOARD_DIR_NAME
+cp dashboards/grafana/prometheus-datasource.yaml $PACKAGE_DIR/$DASHBOARD_DIR_NAME/grafana
+cp dashboards/grafana/dashboards.yaml $PACKAGE_DIR/$DASHBOARD_DIR_NAME/grafana
+cp -r dashboards/grafana/generated-dashboards $PACKAGE_DIR/$DASHBOARD_DIR_NAME/grafana
+tar zcvf $PACKAGE_DIR/$DASHBOARD_DIR_NAME.tar.gz $PACKAGE_DIR/$DASHBOARD_DIR_NAME
+zip $PACKAGE_DIR/$DASHBOARD_DIR_NAME.zip $(tar ztf $PACKAGE_DIR/$DASHBOARD_DIR_NAME.tar.gz)
