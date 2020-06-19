b317e77d6259cbe321d75eef8f02d21927a4770a
Alain Rodriguez <arodrime@gmail.com>
2020-06-19 22:19:04 +0200

    Alain/merge tlp dashboards (#9)
    * Update to Grafana, Add polystat, Allow edit (demo)
    
    * Add overview dashboard for tlp-dashboards
    
    * add C* logo, attempt to add ntpd collection
    
    * Switched latency charts to show histogram_quantile for better aggregations
    
    * attempt to collect ntp metrics
    
    Co-authored-by: Jake Luciani <jake@datastax.com>

--- dashboards/grafana/make-dashboards.sh
+++ dashboards/grafana/make-dashboards.sh
@@ -6,5 +6,5 @@ cd $DIR
 for file in dashboards-jsonnet/*; do
   name=$(basename $file);
   echo "Generating ${name%.jsonnet}.json"
-  docker run -v `pwd`:/here datastax/grafonnet-lib:v0.1.0 jsonnet --ext-str prefix=mcac /here/$file > `pwd`/generated-dashboards/${name%.jsonnet}.json;
+  docker run -v `pwd`:/here datastax/grafonnet-lib:v0.1.1 jsonnet --ext-str prefix=mcac /here/$file > `pwd`/generated-dashboards/${name%.jsonnet}.json;
 done
